09ab8216a3716ef2244a15b50f60ffc7d0815947
Daniel Rose <daniel-rose@gmx.de>
2019-07-03 21:09:22 +0200

    - Allow to expand form tabs for "ProductFormAdd" (over new plugin) - Allow to expand "ProductAbstractTransfer" with submitted data (over new plugin)

--- src/FondOfSpryker/Zed/ProductManagement/Communication/ProductManagementCommunicationFactory.php
+++ src/FondOfSpryker/Zed/ProductManagement/Communication/ProductManagementCommunicationFactory.php
@@ -2,13 +2,40 @@
 
 namespace FondOfSpryker\Zed\ProductManagement\Communication;
 
-use  FondOfSpryker\Zed\ProductManagement\Communication\Transfer\ProductFormTransferMapper;
-use  Spryker\Zed\ProductManagement\Communication\ProductManagementCommunicationFactory as BaseProductManagementCommunicationFactory;
+use FondOfSpryker\Zed\ProductManagement\Communication\Tabs\ProductFormAddTabs;
+use FondOfSpryker\Zed\ProductManagement\Communication\Tabs\ProductFormEditTabs;
+use FondOfSpryker\Zed\ProductManagement\Communication\Transfer\ProductFormTransferMapper;
+use FondOfSpryker\Zed\ProductManagement\ProductManagementDependencyProvider;
+use Spryker\Zed\Gui\Communication\Tabs\TabsInterface;
+use Spryker\Zed\ProductManagement\Communication\ProductManagementCommunicationFactory as BaseProductManagementCommunicationFactory;
 
 class ProductManagementCommunicationFactory extends BaseProductManagementCommunicationFactory
 {
+    public const PLUGINS_PRODUCT_ABSTRACT_FORM_TABS_EXPANDER = 'PLUGINS_PRODUCT_ABSTRACT_FORM_TABS_EXPANDER';
+
     /**
-     * @return \FondOfSpryker\Zed\ProductManagement\Communication\Transfer\ProductFormTransferMapper
+     * @return \Spryker\Zed\Gui\Communication\Tabs\TabsInterface
+     */
+    public function createProductFormAddTabs(): TabsInterface
+    {
+        return new ProductFormAddTabs(
+            $this->getProductAbstractFormTabsExpanderPlugins()
+        );
+    }
+
+    /**
+     * @return \Spryker\Zed\Gui\Communication\Tabs\TabsInterface
+     */
+    public function createProductFormEditTabs(): TabsInterface
+    {
+        return new ProductFormEditTabs(
+            $this->getProductAbstractFormTabsExpanderPlugins(),
+            $this->getProductAbstractFormEditTabsExpanderPlugins()
+        );
+    }
+
+    /**
+     * @return \Spryker\Zed\ProductManagement\Communication\Transfer\ProductFormTransferMapper
      */
     public function createProductFormTransferGenerator()
     {
@@ -18,7 +45,28 @@ class ProductManagementCommunicationFactory extends BaseProductManagementCommuni
             $this->getLocaleFacade(),
             $this->createLocaleProvider(),
             $this->getProductFormTransferMapperExpanderPlugins(),
+            $this->getProductAbstractFormTransferMapperExpanderPlugins(),
             $this->createProductConcreteSuperAttributeFilterHelper()
         );
+    }
+
+    /**
+     * @throws
+     *
+     * @return \FondOfSpryker\Zed\ProductManagement\Dependency\Plugin\ProductAbstractFormTabsExpanderPluginInterface[]
+     */
+    public function getProductAbstractFormTabsExpanderPlugins(): array
+    {
+        return $this->getProvidedDependency(ProductManagementDependencyProvider::PLUGINS_PRODUCT_ABSTRACT_FORM_TABS_EXPANDER);
+    }
+
+    /**
+     * @throws
+     *
+     * @return \FondOfSpryker\Zed\ProductManagement\Dependency\Plugin\ProductAbstractFormTabsExpanderPluginInterface[]
+     */
+    public function getProductAbstractFormTransferMapperExpanderPlugins(): array
+    {
+        return $this->getProvidedDependency(ProductManagementDependencyProvider::PLUGINS_PRODUCT_ABSTRACT_FORM_TRANSFER_MAPPER_EXPANDER);
     }
 }
--- src/FondOfSpryker/Zed/ProductManagement/Communication/Transfer/ProductFormTransferMapper.php
+++ src/FondOfSpryker/Zed/ProductManagement/Communication/Transfer/ProductFormTransferMapper.php
@@ -1,32 +1,69 @@
 <?php
-/**
- * FondOfSpryker Product Management Module Extends Spryker Product Management Module
- *
- * @author Jozsef Geng <gengjozsef86@gmail.com>
- */
+
 namespace FondOfSpryker\Zed\ProductManagement\Communication\Transfer;
 
 use Generated\Shared\Transfer\ProductAbstractTransfer;
-use Spryker\Zed\ProductManagement\Communication\Form\ProductFormAdd;
-use Spryker\Zed\ProductManagement\Communication\Transfer\ProductFormTransferMapper as BaseProductFormTransferMapper;
+use Spryker\Zed\Product\Persistence\ProductQueryContainerInterface;
+use Spryker\Zed\ProductManagement\Communication\Form\DataProvider\LocaleProvider;
+use Spryker\Zed\ProductManagement\Communication\Helper\ProductConcreteSuperAttributeFilterHelperInterface;
+use Spryker\Zed\ProductManagement\Communication\Transfer\ProductFormTransferMapper as SprykerProductFormTransferMapper;
+use Spryker\Zed\ProductManagement\Dependency\Facade\ProductManagementToLocaleInterface;
+use Spryker\Zed\ProductManagement\Persistence\ProductManagementQueryContainerInterface;
+use Symfony\Component\Form\FormInterface;
 
-
-class ProductFormTransferMapper extends BaseProductFormTransferMapper
+class ProductFormTransferMapper extends SprykerProductFormTransferMapper
 {
     /**
-     * @param array $data
+     * @var array|\FondOfSpryker\Zed\ProductManagement\Dependency\Plugin\ProductAbstractFormTransferMapperExpanderPluginInterface[]
+     */
+    protected $productAbstractFormTransferMapperExpanderPlugins;
+
+    /**
+     * @param \Spryker\Zed\Product\Persistence\ProductQueryContainerInterface $productQueryContainer
+     * @param \Spryker\Zed\ProductManagement\Persistence\ProductManagementQueryContainerInterface $productManagementQueryContainer
+     * @param \Spryker\Zed\ProductManagement\Dependency\Facade\ProductManagementToLocaleInterface $localeFacade
+     * @param \Spryker\Zed\ProductManagement\Communication\Form\DataProvider\LocaleProvider $localeProvider
+     * @param \Spryker\Zed\ProductManagementExtension\Dependency\Plugin\ProductFormTransferMapperExpanderPluginInterface[] $productFormTransferMapperExpanderPlugins
+     * @param \FondOfSpryker\Zed\ProductManagement\Dependency\Plugin\ProductAbstractFormTransferMapperExpanderPluginInterface[] $productAbstractFormTransferMapperExpanderPlugins
+     * @param \Spryker\Zed\ProductManagement\Communication\Helper\ProductConcreteSuperAttributeFilterHelperInterface $productConcreteSuperAttributeFilterHelperInterface
+     */
+    public function __construct(
+        ProductQueryContainerInterface $productQueryContainer,
+        ProductManagementQueryContainerInterface $productManagementQueryContainer,
+        ProductManagementToLocaleInterface $localeFacade,
+        LocaleProvider $localeProvider,
+        array $productFormTransferMapperExpanderPlugins,
+        array $productAbstractFormTransferMapperExpanderPlugins,
+        ProductConcreteSuperAttributeFilterHelperInterface $productConcreteSuperAttributeFilterHelperInterface
+    ) {
+        parent::__construct(
+            $productQueryContainer,
+            $productManagementQueryContainer,
+            $localeFacade,
+            $localeProvider,
+            $productFormTransferMapperExpanderPlugins,
+            $productConcreteSuperAttributeFilterHelperInterface
+        );
+
+        $this->productAbstractFormTransferMapperExpanderPlugins = $productAbstractFormTransferMapperExpanderPlugins;
+    }
+
+    /**
+     * @param \Symfony\Component\Form\FormInterface $form
+     * @param int $idProductAbstract
      *
      * @return \Generated\Shared\Transfer\ProductAbstractTransfer
      */
-    protected function createProductAbstractTransfer(array $data)
+    public function buildProductAbstractTransfer(FormInterface $form, $idProductAbstract): ProductAbstractTransfer
     {
-        $productAbstractTransfer = (new ProductAbstractTransfer())
-            ->fromArray($data, true)
-            ->setIdProductAbstract($data[ProductFormAdd::FIELD_ID_PRODUCT_ABSTRACT])
-            ->setSku($data[ProductFormAdd::FIELD_SKU])
-            ->setIdTaxSet($data[ProductFormAdd::FIELD_TAX_RATE]);
+        $productAbstractTransfer = parent::buildProductAbstractTransfer($form, $idProductAbstract);
+
+        $formData = $form->getData();
+
+        foreach ($this->productAbstractFormTransferMapperExpanderPlugins as $plugin) {
+            $productAbstractTransfer = $plugin->map($productAbstractTransfer, $formData);
+        }
 
         return $productAbstractTransfer;
     }
-
 }
